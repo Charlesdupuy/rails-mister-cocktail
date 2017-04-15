cd5b00481d89ddb86e20378088cf1bcc56e0f785
charles dupuy <cdupuy@gmail.com>
2017-04-15 02:18:19 -0300

    vendredi soir-soir

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -8,6 +8,22 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.find(params[:id])
   end
 
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Cocktail.new(ingredient_params)
+    @ingredient.save
+
+    redirect_to ingredient_path(@cocktail)
+  end
+
+  private
+
+  def ingredient_params
+   params.require(:ingredient).permit(:name)
+  end
 
 
 end
